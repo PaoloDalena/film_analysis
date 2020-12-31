d8f0b59d31f09331c09d0d79ee4c5c495cd9a8f2
Paolo Dalena <paolodalena97@gmail.com>
2020-12-31 16:48:07 +0100

    kmeans part

--- 2_clustering/1_kmeans.R
+++ 2_clustering/1_kmeans.R
@@ -1,6 +1,7 @@
 # k means
 library(tibble)
 library(ggpubr)
+library(plotly)
 
 load("2_clustering/films_clus.RData")
 km1 <- kmeans(films_clus, centers = 10, nstart = 100)
@@ -12,6 +13,9 @@ data_num <- films_clus[, c(1, 5, 8, 9, 10, 11, 16)]
 km1 <- kmeans(data_num, centers = 8, nstart = 100)
 pairs(data_num[1:5], col = km1$cluster, pch = 20)
 
+boxplot(data_num)
+# we have very different ranges => let's scale the data
+
 # standardized data
 scdata <- scale(data_num)
 km2 <- kmeans(scdata, centers = 8, nstart = 100)
@@ -21,6 +25,8 @@ pc <- princomp(data_num)
 summary(pc)
 s_pc <- princomp(scdata)
 summary(s_pc)
+# the first two st_princomp are not very informative
+
 # evaluation of the results on the first two principal components
 tbb <- tibble(
   "Dim.1" = pc$scores[, 1],
@@ -40,7 +46,8 @@ ggscatter(tbb,
           size = 0.8,
           ellipse = TRUE,
           ellipse.type = "convex",
-          main = "Unscaled data"
+          main = "Unscaled data",
+          subtitle = "Problem: too much different ranges"
 )
 ggscatter(tbb,
           x = "s_Dim.1", y = "s_Dim.2",
@@ -51,5 +58,17 @@ ggscatter(tbb,
           size = 0.8,
           ellipse = TRUE,
           ellipse.type = "convex",
-          main = "Scaled data"
+          main = "Scaled data",
+          subtitle = "Problem: first two princomp not enough informative"
+)
+
+# let's plot the clustering on the first three principal components
+
+plot_ly(
+  x = s_pc$scores[, 1],
+  y = s_pc$scores[, 2],
+  z = s_pc$scores[, 3],
+  type = "scatter3d",
+  mode = "markers",
+  color = as.factor(km2$cluster)
 )
